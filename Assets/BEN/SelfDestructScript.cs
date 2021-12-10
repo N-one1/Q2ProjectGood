bdfee9c77f1c45a81b87b69647172578d575242a
MUKILTEO\1605505 <Nrichmond2004@gmail.com>
2021-12-10 11:05:24 -0800

    started self destruct code

---
+++ Assets/BEN/SelfDestructScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructScript : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Character(HasAnims)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (this.transform.position.x - Player.transform.position.x > 110)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
