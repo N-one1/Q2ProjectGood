dc4ec525ad672bad26e29faf01d958814b2ff2b2
Hudson Bedward <hudsonbedward@gmail.com>
2021-12-13 13:46:15 -0800

    More Enemy AI Movement and Shooting. I'd say about half way done.

---
+++ Assets/HUDSON/Scripts/HudsonShootEnemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudsonShootEnemy : MonoBehaviour
+{
+    public float range;
+    private float distToPlayer;
+
+    [HideInInspector]
+
+    public Transform player;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if(distToPlayer <= range)
+        {
+            if(player.position.x > transform.position.x && transform.localScale.x < 0
+                || player.position.x < transform.position.x && transform.localScale.x > 0)
+
+            {
+                //Flip();
+            }
+
+            Shoot();
+        }
+
+    }
+    private void Shoot()
+    {
+
+    }
+
+}
