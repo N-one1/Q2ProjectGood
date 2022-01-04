56de31510182328036007330db5457c4d88f7339
MUKILTEO\1605505 <Nrichmond2004@gmail.com>
2022-01-04 13:27:01 -0800

    Bite and start of enemy spawning

---
+++ Assets/NATHAN/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    [SerializeField] private List<Transform> EnemyList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        Transform chosenEnemy = EnemyList[Random.Range(0, EnemyList.Count)];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Transform SpawnEnemy(Transform chosenEmemy, Vector3 spawnPosition)
+    {
+        Transform Instantiate(chosenEnemy, spawnPosition, Quaternion.identity);
+    }
+}
