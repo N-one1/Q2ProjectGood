961bae6e8c121d963e7b8035fca6965654737ba6
Hudson Bedward <hudsonbedward@gmail.com>
2022-01-06 14:28:26 -0800

    Progress on Main Menu

---
+++ Assets/HUDSON/Scripts/HudsMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HudsMainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //or can use SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("TESTGameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
