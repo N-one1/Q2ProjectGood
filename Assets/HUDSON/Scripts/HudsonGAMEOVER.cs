ed4fb791a760167eee93b796d75e3896ef97e7ba
Hudson Bedward <hudsonbedward@gmail.com>
2022-01-12 13:41:34 -0800

    Done with menu and now working on game over screen

---
+++ Assets/HUDSON/Scripts/HudsonGAMEOVER.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HudsonGAMEOVER : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //or can use SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("MainScene");
+    }
+
+
+    public void GoToCreditsMenu()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Menu UI");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
