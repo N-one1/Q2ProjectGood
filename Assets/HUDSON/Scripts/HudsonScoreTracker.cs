382e6c3d25a35bb97e792bebdd4c6c6b52ef754b
Hudson Bedward <hudsonbedward@gmail.com>
2022-01-19 12:36:26 -0800

    Stuff done in my own scene

---
+++ Assets/HUDSON/Scripts/HudsonScoreTracker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudsonScoreTracker : MonoBehaviour
+{
+    public Transform player;
+    public float score;
+    private float distance;
+    public Text scoreText;
+    public float scoreTrue;
+    public float points;
+
+    public GameObject Player;
+    public PlayerHealth PH;
+    public float hp;
+
+    //health text
+    public Text healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Character(HasAnims)");
+
+        PH = Player.GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = player.transform.position.x;
+        
+        if (score < distance)
+        {
+            score++;
+        }
+
+        scoreTrue = score + points;
+
+        hp = PH.HP;
+
+        scoreText.text = " " + scoreTrue;
+        healthText.text = " " + hp;
+    }
+}
